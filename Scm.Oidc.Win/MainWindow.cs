5b16337cba0194b17a25e4bc9404ee9e356db3ba
zero <avenyao@qq.com>
2025-01-04 20:35:43 +0800

    类名调整。

--- Scm.Oidc.Win/MainWindow.cs
+++ Scm.Oidc.Win/MainWindow.cs
@@ -1,10 +1,10 @@
 namespace Com.Scm.Oidc.Win
 {
-    public partial class Form1 : Form
+    public partial class MainWindow : Form
     {
         private OidcClient _Client;
 
-        public Form1()
+        public MainWindow()
         {
             InitializeComponent();
 
